79433474109096ba9058ea6c6d231691abb8f911
nguyen-khac-hai-lam <lamnkh2610@gmail.com>
2025-09-30 20:56:43 +0700

    1.2

---
+++ oop/HW1/SumOfTwoDice.java
@@ -0,0 +1,7 @@
+public class SumOfTwoDice {
+    public static void main(String[] args) {
+        int a=1+(int)(Math.random()*6);
+        int b=1+(int)(Math.random()*6);
+        System.out.println(a+b);
+    }
+}
