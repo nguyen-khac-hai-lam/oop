304fa28df682bb54ee5716ef339e1c4c2662d1e1
nguyen-khac-hai-lam <lamnkh2610@gmail.com>
2025-09-30 20:52:58 +0700

    1.2

---
+++ oop/HW1/Distance.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=sc.nextInt();
+        System.out.println(Math.sqrt(a*a+b*b));
+    }
+}
